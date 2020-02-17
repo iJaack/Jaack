e9b5a80e78caeebf9c61441d76c654a0cb19ca7b
Giacomo Barbieri <giacomobarbieri94@gmail.com>
2020-02-17 23:47:51 +0100

    Improved and cleaned

--- assets/js/beeline-reader.js
+++ assets/js/beeline-reader.js
@@ -1,5 +1,13 @@
-var elements = document.querySelectorAll(".color");
+var elements = document.querySelectorAll("");
 for (var i = 0; i < elements.length; i++) {
-  var beeline = new BeeLineReader(elements[i], { theme: "bright" });
+  var beeline = new BeeLineReader(elements[i], {
+    theme: ' night_blues',
+    autoNightMode: true,
+    skipBackgroundColor: true,
+    customColor1: '#FFFFFF',
+    customColor2: '#0000FF',
+    customColor3: '#FFFFFF',
+    customColor4: '#FF0500',
+  });
   beeline.color();
 }
